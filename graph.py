325985ddf62d3963b8a482b6f834db9f8e5b5b5d
Nikita Rubocki <nikita.rubocki@gmail.com>
2022-02-19 11:41:46 -0800

    Create some graphing results

--- graph.py
+++ graph.py
@@ -10,34 +10,43 @@ plot_path = "./plots/"
 df = pd.read_csv("overall_results.csv")
 
 # Heatmap
-# observs = [10, 100, 1000, 10000]
-# for obs in observs:
-#     obs_df = df.loc[df['obs'] == obs]
-#     heatmap_df = obs_df.pivot('feats', 'imp_ratio', 'overlap')
-#     # print(heatmap_df)
-#     plt.figure()
-#     sns.heatmap(heatmap_df, annot=True, cmap="BuPu")
-#     plt.savefig(plot_path+f"obs{obs}_heatmap.png")
-# print("Heatmaps generated!")
+observs = [10, 100, 1000, 10000]
+for obs in observs:
+    obs_df = df.loc[df['obs'] == obs]
+    heatmap_df = obs_df.pivot('feats', 'imp_ratio', 'overlap')
+    plt.figure()
+    plt.title("Overlap")
+    sns.heatmap(heatmap_df, annot=True, cmap="BuPu")
+    plt.savefig(plot_path+f"obs{obs}_olap_heatmap.png")
+print("Overlap heatmaps generated!")
 
-# # 3D Plot
-# # axes instance
-# fig = plt.figure(figsize=(6,6))
-# ax = Axes3D(fig, auto_add_to_figure=False)
-# fig.add_axes(ax)
+for obs in observs:
+    obs_df = df.loc[df['obs'] == obs]
+    heatmap_df = obs_df.pivot('feats', 'imp_ratio', 'distance')
+    plt.figure()
+    plt.title("Distance")
+    sns.heatmap(heatmap_df, annot=True, cmap="BuPu")
+    plt.savefig(plot_path+f"obs{obs}_dist_heatmap.png")
+print("Distance heatmaps generated!")
 
-# # get colormap from seaborn
-# cmap = ListedColormap(sns.color_palette("husl", 256).as_hex())
+# 3D Plot
+# axes instance
+fig = plt.figure(figsize=(6,6))
+ax = Axes3D(fig, auto_add_to_figure=False)
+fig.add_axes(ax)
 
-# # plot
-# sc = ax.scatter(df['feats'], df['imp_ratio'], df['overlap'], c=df['overlap'], cmap=cmap, alpha=1)
-# ax.set_xlabel('# of Features')
-# ax.set_ylabel('Importance Ratio')
-# ax.set_zlabel('Overlap')
+# get colormap from seaborn
+cmap = ListedColormap(sns.color_palette("husl", 256).as_hex())
 
-# # legend
-# plt.legend(*sc.legend_elements(), bbox_to_anchor=(1.05, 1), loc=2)
+# plot
+sc = ax.scatter(df['feats'], df['imp_ratio'], df['obs'], c=df['overlap'], cmap=cmap, alpha=1)
+ax.set_xlabel('# of Features')
+ax.set_ylabel('Importance Ratio')
+ax.set_zlabel('Overlap')
 
-# # save
-# plt.savefig(plot_path+"scatter_plot.png", bbox_inches='tight')
-# print("Scatterplot generated!")
+# legend
+plt.legend(*sc.legend_elements(), bbox_to_anchor=(1.05, 1), loc=2)
+
+# save
+plt.savefig(plot_path+"scatter_plot.png", bbox_inches='tight')
+print("Scatterplot generated!")
