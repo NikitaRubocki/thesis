e1f5d35a10f234ceced018f0a91a8c3216d099a0
NikitaRubocki <nikita.rubocki@gmail.com>
2022-02-21 14:27:50 -0700

    Move old experiment files into folder

--- app.py
+++ app.py
@@ -1,33 +1,27 @@
+import pandas as pd
+import plotly.express as px
+
+# Load dataframe
+df = pd.read_csv("overall_results.csv")
+
+fig = px.scatter_3d(
+    df, 
+    x='feats', 
+    y='imp_ratio', 
+    z='obs',
+    # color='overlap', 
+    color='distance', 
+)
+fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
+
+
 import dash
 from dash import dcc
 from dash import html
-from dash.dependencies import Input, Output
-import plotly.express as px
 
-df = px.data.iris()
-
-app = dash.Dash(__name__)
-
+app = dash.Dash()
 app.layout = html.Div([
-    dcc.Graph(id="scatter-plot"),
-    html.P("Petal Width:"),
-    dcc.RangeSlider(
-        id='range-slider',
-        min=0, max=2.5, step=0.1,
-        marks={0: '0', 2.5: '2.5'},
-        value=[0.5, 2]
-    ),
+    dcc.Graph(figure=fig)
 ])
 
-@app.callback(
-    Output("scatter-plot", "figure"), 
-    [Input("range-slider", "value")])
-def update_bar_chart(slider_range):
-    low, high = slider_range
-    mask = (df.petal_width > low) & (df.petal_width < high)
-
-    fig = px.scatter_3d(df[mask], 
-        x='sepal_length', y='sepal_width', z='petal_width',
-        color="species", hover_data=['petal_width'])
-    return fig
-app.run_server(debug=True)
+app.run_server(debug=True, use_reloader=False)
