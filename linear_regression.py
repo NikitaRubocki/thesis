1c01be2b616072300ec4a94c65279d51af545dd2
NikitaRubocki <nikita.rubocki@gmail.com>
2022-03-07 16:20:50 -0800

    Add distance as possible target label

--- linear_regression.py
+++ linear_regression.py
@@ -2,11 +2,11 @@ import pandas as pd
 import statsmodels.api as sm
 
 # Load dataframe
-df = pd.read_csv("overall_results.csv")
+df = pd.read_csv("./experiment02_25_22/overall_results.csv")
 
 # Split the data
 # Y = df['overlap']
-Y = df['overlap']
+Y = df['distance']
 X = df[['feats', 'imp_ratio', 'obs']]
 X = sm.add_constant(X)
 # print(X)
